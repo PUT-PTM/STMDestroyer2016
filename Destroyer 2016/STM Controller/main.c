d89d65d66df0a2d4120e5100127729249011a015
DominikDeveloper <dominik.garczyk@student.put.poznan.pl>
2016-04-18 13:41:44 +0200

    STM Gamepad Mode
    Blue button acts like gamepad button. Rotating of board around X-axis
    equals in effect to movement of left analog stick

--- Destroyer 2016/STM Controller/main.c
+++ Destroyer 2016/STM Controller/main.c
@@ -16,6 +16,65 @@
 #include "tm_stm32f4_delay.h"
 #include "tm_stm32f4_disco.h"
 
+#include "stm32f4_discovery_lis302dl.h"
+#include "stm32f4xx_spi.h"
+
+int8_t acc_x, acc_y, acc_z;
+
+void LIS302DL_Init1()
+{
+        LIS302DL_InitTypeDef LIS302DL_InitStruct;
+
+        /*Set configuration of LIS302DL*/
+
+        LIS302DL_InitStruct.Power_Mode=LIS302DL_LOWPOWERMODE_ACTIVE;
+
+        LIS302DL_InitStruct.Output_DataRate=LIS302DL_DATARATE_100;
+
+        LIS302DL_InitStruct.Axes_Enable = LIS302DL_X_ENABLE | LIS302DL_Y_ENABLE | LIS302DL_Z_ENABLE;
+
+        LIS302DL_InitStruct.Full_Scale=LIS302DL_FULLSCALE_2_3;
+
+        LIS302DL_InitStruct.Self_Test=LIS302DL_SELFTEST_NORMAL;
+
+        LIS302DL_Init(&LIS302DL_InitStruct);
+
+        //int i;
+        //for (i=0;i<10000;i++);
+}
+
+int8_t left_joystick()
+{
+				LIS302DL_Read(&acc_x,LIS302DL_OUT_X_ADDR,1);
+				LIS302DL_Read(&acc_y,LIS302DL_OUT_Y_ADDR,1);
+				LIS302DL_Read(&acc_z,LIS302DL_OUT_Z_ADDR,1);
+
+				int8_t x = 0;
+
+				if(acc_y > 20)
+				{
+					/* Simulate left stick rotation */
+					x = 50; /* X axis */
+					//Gamepad1.LeftYAxis = 30; /* Y axis */
+				}
+				if(acc_y < -20)
+				{
+					/* Simulate left stick rotation */
+					x = -50; /* X axis */
+					//Gamepad1.LeftYAxis = 30; /* Y axis */
+				}
+				if(acc_y <= 20 && acc_y >= -20)
+				{
+					/* Simulate left stick rotation */
+					x = 0; /* X axis */
+					//Gamepad1.RightXAxis = 0; /* X axis */
+					//Gamepad1.LeftYAxis = 30; /* Y axis */
+				}
+
+				return x;
+
+}
+
 int main(void) {
 	uint8_t already = 0;
 
@@ -26,6 +85,12 @@ int main(void) {
 
 	/* Initialize system */
 	SystemInit();
+
+	/* Initialize LIS302DL - acceleration sensor;
+	 * the board position determines the movement of joystick
+	 * --(own)
+	 * */
+	LIS302DL_Init1();
 
 	/* Initialize leds */
 	TM_DISCO_LedInit();
@@ -59,24 +124,54 @@ int main(void) {
 			if (TM_DISCO_ButtonPressed() && already == 0) { /* Button on press */
 				already = 1;
 
-				/* Set pressed keys = WIN + R */
-				Keyboard.L_GUI = TM_USB_HIDDEVICE_Button_Pressed;	/* Win button */
-				Keyboard.Key1 = 0x15; 								/* R */
-				/* Result = "Run" command */
+				/* Gamepad 1 */
+				/* Simulate button 1 on gamepad 1 */
+				Gamepad1.Button1 = TM_USB_HIDDEVICE_Button_Pressed;
 
-				/* Send keyboard report */
-				TM_USB_HIDDEVICE_KeyboardSend(&Keyboard);
+				/* Send report for gamepad 1 */
+				TM_USB_HIDDEVICE_GamepadSend(TM_USB_HIDDEVICE_Gamepad_Number_1, &Gamepad1);
+
+
 			} else if (!TM_DISCO_ButtonPressed() && already == 1) { /* Button on release */
 				already = 0;
 
-				/* Release all buttons */
-				Keyboard.L_GUI = TM_USB_HIDDEVICE_Button_Released;	/* No button */
-				Keyboard.Key1 = 0x00; 								/* No key */
-				/* Result = Released everything */
+				//I think the command that is 5 lines below is sufficient but no...
+				//I release blue button but computer "thinks" I still press it.
+				Gamepad1.Button1 = TM_USB_HIDDEVICE_Button_Released;
 
-				/* Send keyboard report */
-				TM_USB_HIDDEVICE_KeyboardSend(&Keyboard);
+				/* Send command to release all buttons on both gamepads */
+				TM_USB_HIDDEVICE_GamepadReleaseAll(TM_USB_HIDDEVICE_Gamepad_Number_1);
 			}
+
+
+			/* Read the board position on axes
+			 * --(own)
+			 * */
+			LIS302DL_Read(&acc_x,LIS302DL_OUT_X_ADDR,1);
+			LIS302DL_Read(&acc_y,LIS302DL_OUT_Y_ADDR,1);
+			LIS302DL_Read(&acc_z,LIS302DL_OUT_Z_ADDR,1);
+
+			/* The value on axis depends on board lean
+			 * HERE: Changing orientation of left joystick under the influence of X-axis
+			* --(own)
+			* */
+			if(acc_x > 40)
+			{
+				/* Simulate left stick rotation */
+				Gamepad1.LeftXAxis = -127; /* X axis */
+			}
+			if(acc_x < -40)
+			{
+				/* Simulate left stick rotation */
+				Gamepad1.LeftXAxis = 127; /* X axis */
+			}
+			if(acc_x <= 40 && acc_x >= -40)
+			{
+				/* Simulate left stick rotation */
+				Gamepad1.LeftXAxis = NULL; /* X axis */
+			}
+
+			TM_USB_HIDDEVICE_GamepadSend(TM_USB_HIDDEVICE_Gamepad_Number_1, &Gamepad1);
 
 /* Simple sketch end */
 
